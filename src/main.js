64dd7049209bda4ce54f384b159dc1ee1269145e
Jibran Kalia <jkalia@student.42.us.org>
2017-09-03 16:44:06 -0700

    restart

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 var aiStart = require('ai.start');
 var aiSpawn = require('ai.spawn');
 var aiRun = require('ai.run');
+var aiPlan = require('ai.plan');
 
 module.exports.loop = function () {
     aiStart.init();
+    aiPlan.plan();
     aiSpawn.spawn();
     aiRun.run();
+
 };
